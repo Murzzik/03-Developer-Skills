92b29c3a9d11d5c66d2af0d76c3df994a271a83d
Murzzik <mcalexstar@gmail.com>
2022-01-31 13:07:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -46,4 +46,25 @@ calcTempAmplitude([3, 7, 4, 1, 5, 8])
 // - With 2 arrays, should we implement functionality twice? NO!
 // Just merge two arrays
 // 2) Breaking up into sub-problems
-// - How to merge 2 arrays?
+// - Merge 2 arrays
+
+const calcTempAmplitudeNew = function (t1, t2) {
+
+    const temps = t1.concat(t2) // create new array to cocncat t1.arr & t2.arr
+    console.log(temps)
+
+    let max = temps[0];
+    let min = temps[0];
+
+    for (let i = 0; i < temps.length; i++) {
+        const curTemp = temps[i]
+        if (typeof curTemp !== 'number') continue;
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min;
+}
+const amplitudeNew = calcTempAmplitudeNew([3,5,7], [4,5,6,8])
+console.log(amplitudeNew)
+calcTempAmplitude([3, 7, 4, 1, 5, 8]);
